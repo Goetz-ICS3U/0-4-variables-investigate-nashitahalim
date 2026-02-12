d837e831a2beeda3237b7edf87fa3de038343d5c
nashitahalim <354230677@educ.dpcdsb.org>
2026-02-12 12:05:39 -0500

    Hi

---
+++ var.investigate.py
@@ -0,0 +1,36 @@
+"""
+author:
+date:
+Investigating Variables
+"""
+
+# Input
+name = "Nashita"
+grade = 11
+favourite_exclamation = "sis"
+least_favourite_colour = "gray"
+is_cool = True
+math_test_score = 92
+sister_name = "Arshi"
+
+slope = 6
+y_intercept = -5
+x = 0
+y = slope * x + y_intercept
+
+friend = "Dareen"
+food = "dumplings"
+place = "NoFrills"
+
+
+# Processing / Output
+print(
+    f"Hey guys, my name is {name} and I am currently in grade {grade}. "
+    + f"Yesterday, there was a spider in the classroom that made me yell {favourite_exclamation} whiile I was taking a math test with my {least_favourite_colour} pen :/ "
+    + f"One of the questions on the test was wether or not I thought Elvis was cool, so I obviously put {is_cool} to get the answer right... "
+    + f"Turns out that I only scored a {math_test_score} on the test. One of the qusetions was asking me to write the equation for a line "
+    + f"with a slope of {slope} and a y-intercept of {y_intercept} so I wrote y = {slope}x + {y_intercept}, but I did not notice the x value of {x} in the question "
+    + f"so I needed to, on the next line, say that the value of y was {y}. "
+    + f"I told my sister {sister_name} about this and she just laughed at me 🙄"
+    + f"Anyway, that's the story of my math test. Lowkey a weird day, but at least I went out with {friend} after. We went to {place} and got some {food}! It tasted funky, but overall good."
+)
